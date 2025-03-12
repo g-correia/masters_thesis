e83651f8363f55b2449442af543a69ac6f8f28f1
Gabriela <75147647+g-correia@users.noreply.github.com>
2025-03-12 16:47:36 +0100

    update tabulations.R
    adjusting preliminary table and fixing mistakes

--- tabulations.R
+++ tabulations.R
@@ -13,85 +13,85 @@ library(kableExtra)
 # Type 1 error
 
 # Strategy 1: Unadjusted
-(a5.1 <- type_1_powered_1[[2]][1]) # A 10%/10%
+(a1.5 <- type_1_powered_1[[2]][1]) # A 10%/10%
 #(a52 <- type_1_powered_1[[2]][2]) # B                              
-(a6.1 <- type_1_powered_2[[2]][1]) # A 10%/30%
+(a1.6 <- type_1_powered_2[[2]][1]) # A 10%/30%
 #(a52 <- type_1_powered_2[[2]][2]) # B                              
-(a5.2 <- type_1_powered_3[[2]][1]) # A 20%/10%
+(a2.5 <- type_1_powered_3[[2]][1]) # A 20%/10%
 #(a52 <- type_1_powered_3[[2]][2]) # B                         
-(a6.2 <- type_1_powered_4[[2]][1]) # A 20%/30%
+(a2.6 <- type_1_powered_4[[2]][1]) # A 20%/30%
 #(a52 <- type_1_powered_4[[2]][2]) # B                      
 
 # Strategy 2: Adjusted to account for single pivotal study
-(a7.1 <- type_1_powered_1_st2[[2]][1]) # A 10%/10%
+(a1.7 <- type_1_powered_1_st2[[2]][1]) # A 10%/10%
 #(a32 <- type_1_powered_1_st2[[2]][2]) # B
-(a8.1 <- type_1_powered_2_st2[[2]][1]) # A 10%/30%
+(a1.8 <- type_1_powered_2_st2[[2]][1]) # A 10%/30%
 #(a42 <- type_1_powered_2_st2[[2]][2]) # B
-(a7.2 <- type_1_powered_3_st2[[2]][1]) # A 20%/10%
+(a2.7 <- type_1_powered_3_st2[[2]][1]) # A 20%/10%
 #(a <- type_1_powered_3_st2[[2]][2]) # B
-(a8.2 <- type_1_powered_4_st2[[2]][1]) # A 20%/30%
+(a2.8 <- type_1_powered_4_st2[[2]][1]) # A 20%/30%
 #(a <- type_1_powered_4_st2[[2]][2]) # B
 
 # Strategy 3: Bonferroni correction
-(a9.1 <- type_1_powered_1_st3[[2]][1]) # A 10%/10%
+(a1.9 <- type_1_powered_1_st3[[2]][1]) # A 10%/10%
 #(a32 <- type_1_powered_1_st3[[2]][2]) # B
-(a10.1 <- type_1_powered_2_st3[[2]][1]) # A 10%/30%
+(a1.10 <- type_1_powered_2_st3[[2]][1]) # A 10%/30%
 #(a42 <- type_1_powered_2_st3[[2]][2]) # B
-(a9.2 <- type_1_powered_3_st3[[2]][1]) # A 20%/10%
+(a2.9 <- type_1_powered_3_st3[[2]][1]) # A 20%/10%
 #(a <- type_1_powered_3_st3[[2]][2]) # B
-(a10.2 <- type_1_powered_4_st3[[2]][1]) # A 20%/30%
+(a2.10 <- type_1_powered_4_st3[[2]][1]) # A 20%/30%
 #(a <- type_1_powered_4_st3[[2]][2]) # B
 
 # Strategy 4: Adjusted to account for single pivotal study and Boferroni correction
-(a11.1 <- type_1_powered_1_st4[[2]][1]) # A 10%/10%
+(a1.11 <- type_1_powered_1_st4[[2]][1]) # A 10%/10%
 #(a32 <- type_1_powered_1_st4[[2]][2]) # B
-(a12.1 <- type_1_powered_2_st4[[2]][1]) # A 10%/30%
+(a1.12 <- type_1_powered_2_st4[[2]][1]) # A 10%/30%
 #(a42 <- type_1_powered_2_st4[[2]][2]) # B
-(a11.2 <- type_1_powered_3_st4[[2]][1]) # A 20%/10%
+(a2.11 <- type_1_powered_3_st4[[2]][1]) # A 20%/10%
 #(a <- type_1_powered_3_st4[[2]][2]) # B
-(a12.2 <- type_1_powered_4_st4[[2]][1]) # A 20%/30%
+(a2.12 <- type_1_powered_4_st4[[2]][1]) # A 20%/30%
 #(a <- type_1_powered_4_st4[[2]][2]) # B
 
 # power
 
 # Strategy 1: Unadjusted
-(a5.3 <- power_1[[2]][1]) # A 10%/10%
+(a3.5 <- power_1[[2]][1]) # A 10%/10%
 #(a62 <- power_1[[2]][2]) # B               
-(a6.3 <- power_2[[2]][1]) # A 10%/30%
+(a3.6 <- power_2[[2]][1]) # A 10%/30%
 #(a62 <- power_2[[2]][2]) # B               
-(a5.4 <- power_3[[2]][1]) # A 20%/10%
+(a4.5 <- power_3[[2]][1]) # A 20%/10%
 #(a82 <- power_3[[2]][2]) # B               
-(a6.4 <- power_4[[2]][1]) # A 20%/30%
+(a4.6 <- power_4[[2]][1]) # A 20%/30%
 #(a82 <- power_4[[2]][2]) # B               
 
 # Strategy 2: Adjusted to account for single pivotal study
-(a7.3 <- power_1_st2[[2]][1]) # A 10%/10%
+(a3.7 <- power_1_st2[[2]][1]) # A 10%/10%
 #(a52 <- power_1_st2[[2]][2]) # B               
-(a8.3 <- power_2_st2[[2]][1]) # A 10%/30%
+(a3.8 <- power_2_st2[[2]][1]) # A 10%/30%
 #(a62 <- power_2_st2[[2]][2]) # B               
-(a7.4 <- power_3_st2[[2]][1]) # A 20%/10%
+(a4.7 <- power_3_st2[[2]][1]) # A 20%/10%
 #(a72 <- power_3_st2[[2]][2]) # B               
-(a8.4 <- power_4_st2[[2]][1]) # A 20%/30%
+(a4.8 <- power_4_st2[[2]][1]) # A 20%/30%
 #(a82 <- power_4_st2[[2]][2]) # B               
 
 # Strategy 3: Bonferoni correction
-(a9.3 <- power_1_st3[[2]][1]) # A 10%/10%
+(a3.9 <- power_1_st3[[2]][1]) # A 10%/10%
 #(a92 <- power_1_st3[[2]][2]) # B               
-(a10.3 <- power_2_st3[[2]][1]) # A 10%/30%
+(a3.10 <- power_2_st3[[2]][1]) # A 10%/30%
 #(a10 <- power_2_st3[[2]][2]) # B               
-(a9.4 <- power_3_st3[[2]][1]) # A 20%/10%
+(a4.9 <- power_3_st3[[2]][1]) # A 20%/10%
 #(a11 <- power_3_st3[[2]][2]) # B               
-(a10.4 <- power_4_st3[[2]][1]) # A 20%/30%
+(a4.10 <- power_4_st3[[2]][1]) # A 20%/30%
 #(a12 <- power_4_st3[[2]][2]) # B               
 
 # Strategy 4: Adjusted to account for single pivotal study and Boferroni correction
-(a11.3 <- power_1_st4[[2]][1]) # A 10%/10%
+(a3.11 <- power_1_st4[[2]][1]) # A 10%/10%
 #(a92 <- power_1_st4[[2]][2]) # B               
-(a12.3 <- power_2_st4[[2]][1]) # A 10%/30%
+(a3.12 <- power_2_st4[[2]][1]) # A 10%/30%
 #(a10 <- power_2_st4[[2]][2]) # B               
-(a11.4 <- power_3_st4[[2]][1]) # A 20%/10%
+(a4.11 <- power_3_st4[[2]][1]) # A 20%/10%
 #(a11 <- power_3_st4[[2]][2]) # B               
-(a12.4 <- power_4_st4[[2]][1]) # A 20%/30%
+(a4.12 <- power_4_st4[[2]][1]) # A 20%/30%
 #(a12 <- power_4_st4[[2]][2]) # B               
 
 # FDR TDR
@@ -103,31 +103,31 @@ library(kableExtra)
 #(a<-discovery_1_2[[2]][2]) # TDR
 
 (a5.5<-discovery_2_1[[2]][1]) # FDR A=0/B=10 10%/10%
-(a5.6<-discovery_2_1[[2]][2]) # TDR
-(a6.5<-discovery_2_2[[2]][1]) # FDR A=0/B=10 10%/30%
+(a6.5<-discovery_2_1[[2]][2]) # TDR
+(a5.6<-discovery_2_2[[2]][1]) # FDR A=0/B=10 10%/30%
 (a6.6<-discovery_2_2[[2]][2]) # TDR     
 
-(a5.7<-discovery_3_1[[2]][1]) # FDR A=0/B=20 10%/10%
-(a5.8<-discovery_3_1[[2]][2]) # TDR                                
-(a6.7<-discovery_3_2[[2]][1]) # FDR A=0/B=20 10%/30%
-(a6.8<-discovery_3_2[[2]][2]) # TDR  
+(a7.5<-discovery_3_1[[2]][1]) # FDR A=0/B=20 10%/10%
+(a8.5<-discovery_3_1[[2]][2]) # TDR                                
+(a7.6<-discovery_3_2[[2]][1]) # FDR A=0/B=20 10%/30%
+(a8.6<-discovery_3_2[[2]][2]) # TDR  
 
 # omitted: 4/9 Treatment A has treatment effect (Δ) of 10% vs Treatment B has no effect (treatment effect, Δ = 0%)
 
-(a5.9<-discovery_5_1[[2]][1]) # FDR A=10/B=10 10%/10%
-(a5.10<-discovery_5_1[[2]][2]) # TDR                                           
-(a6.9<-discovery_5_2[[2]][1]) # FDR A=10/B=10 10%/30%
-(a6.10<-discovery_5_2[[2]][2]) # TDR
+(a9.5<-discovery_5_1[[2]][1]) # FDR A=10/B=10 10%/10%
+(a10.5<-discovery_5_1[[2]][2]) # TDR                                           
+(a9.6<-discovery_5_2[[2]][1]) # FDR A=10/B=10 10%/30%
+(a10.6<-discovery_5_2[[2]][2]) # TDR
 
-(a5.11<-discovery_6_1[[2]][1]) # FDR A=10/B=20 10%/10%
-(a5.12<-discovery_6_1[[2]][2]) # TDR                                        
-(a6.11<-discovery_6_2[[2]][1]) # FDR A=10/B=20 10%/30%
-(a6.12<-discovery_6_2[[2]][2]) # TDR   
+(a11.5<-discovery_6_1[[2]][1]) # FDR A=10/B=20 10%/10%
+(a12.5<-discovery_6_1[[2]][2]) # TDR                                        
+(a11.6<-discovery_6_2[[2]][1]) # FDR A=10/B=20 10%/30%
+(a12.6<-discovery_6_2[[2]][2]) # TDR   
 
-(a5.13<-discovery_6_3[[2]][1]) # FDR A=10/B=20 20%/10%
-(a5.14<-discovery_6_3[[2]][2]) # TDR                                    
-(a6.13<-discovery_6_4[[2]][1]) # FDR A=10/B=20 20%/30%
-(a6.14<-discovery_6_4[[2]][2]) # TDR     
+(a13.5<-discovery_6_3[[2]][1]) # FDR A=10/B=20 20%/10%
+(a14.5<-discovery_6_3[[2]][2]) # TDR                                    
+(a13.6<-discovery_6_4[[2]][1]) # FDR A=10/B=20 20%/30%
+(a14.6<-discovery_6_4[[2]][2]) # TDR     
 
 # omitted: 7/9 Treatment A has treatment effect (Δ) of 20% vs Treatment B has treatment effect (Δ) of 0%
 # omitted: 8/9 Treatment A has treatment effect (Δ) of 20% vs Treatment B has treatment effect (Δ) of 10%
@@ -142,8 +142,8 @@ library(kableExtra)
 ### Temporary break in code for intermediate test table ###
 ###########################################################
 {
-Column5 = round(c(a5.1, a5.2, a5.3, a5.4, NA, NA, a5.7, a5.8, a5.9, a5.10, a5.11, a5.12, a5.13, a5.14, NA, NA),4)
-Column6 = round(c(a6.1, a6.2, a6.3, a6.4, NA, NA, a6.7, a6.8, a6.9, a6.10, a6.11, a6.12, a6.13, a6.14, NA, NA),4)
+Column5 = round(c(a1.5, a2.5, a3.5, a4.5, NA, NA, a5.5, a6.5, a7.5, a8.5, a9.5, a10.5, a11.5, a12.5, a13.5, a14.5, NA,NA),4)
+Column6 = round(c(a1.6, a2.6, a3.6, a4.6, NA, NA, a5.6, a6.6, a7.6, a8.6, a9.6, a10.6, a11.6, a12.6, a13.6, a14.6, NA,NA),4)
 
 n_rows <- 16
 n_cols <- 19
@@ -154,8 +154,8 @@ df <- as.data.frame(matrix("", nrow = n_rows, ncol = n_cols), stringsAsFactors =
 # First column for row names and then columns 2-19
 colnames(df) <- c("Response Rate (%)", rep(c("10","30"), 9))
 
-df[, 5] <- Column5*100
-df[, 6] <- Column6*100
+df[, 4] <- Column5*100
+df[, 5] <- Column6*100
 
 # Define row names:
 df[,1] <- c(
@@ -163,9 +163,9 @@ df[,1] <- c(
   "Type I Error (%) (Powered to $\\Delta$ = 20%)",
   "Power (%) ($\\Delta$ = 10%)",
   "Power (%) ($\\Delta$ = 20%)",
-  "Treatment no effect
+  "Treatment no effect \\\\
   Treatment no effect",
-  "Treatment no effect
+  "Treatment no effect \\\\
   Treatment no effect",
   "Treatment no effect
   Treatment $\\Delta$ = 10%",
@@ -200,7 +200,7 @@ middle_header <- c("Adjustment" = 1, "Unadjusted" = 2, "Bonferroni Correction" =
 
 
 # Create an HTML preview table so you can view and adjust it in RStudio
-table_html <- kable(df, format = "html", caption = "Complex Table with Merged Row Names", align = "c") %>%
+table_html <- kable(df, format = "html", caption = "Results table", align = "c") %>%
   add_header_above(middle_header) %>%
   add_header_above(top_header) %>%
   collapse_rows(columns = 1, valign = "middle") %>% 
